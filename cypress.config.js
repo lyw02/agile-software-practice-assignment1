1538b92a6838664f155c0254259e643e46be8fdf
Yiwei Liu <2814824250@qq.com>
2023-11-26 12:08:34 +0000

    Cypress Component Testing - setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
   e2e: {
-    supportFile: 'cypress/support/index.js',
-    "baseUrl": "http://localhost:3000/",
-    "viewportWidth": 1980,
-    "viewportHeight": 1080,
+    supportFile: "cypress/support/index.js",
+    baseUrl: "http://localhost:3000/",
+    viewportWidth: 1980,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });
